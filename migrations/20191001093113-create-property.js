dc5b223fbe49a3cf8a2965de6195d6b06e8b9f83
Anuraag <thehray@gmail.com>
2019-10-01 22:03:07 +0100

    + property model added

---
+++ migrations/20191001093113-create-property.js
@@ -0,0 +1,49 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("properties", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      lat: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        required: true
+      },
+      lng: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        required: true
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      zip: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("properties");
+  }
+};
---
+++ models/property.js
@@ -0,0 +1,34 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const property = sequelize.define(
+    "property",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      name: DataTypes.STRING,
+      lat: { type: DataTypes.FLOAT, allowNull: false, required: true },
+      lng: { type: DataTypes.FLOAT, allowNull: false, required: true },
+      address: DataTypes.STRING,
+      city: DataTypes.STRING,
+      country: DataTypes.STRING,
+      zip: DataTypes.STRING,
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    },
+    {}
+  );
+  property.associate = function(models) {
+    // associations can be defined here
+  };
+  return property;
+};
