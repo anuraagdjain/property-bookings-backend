b686fefa9444c7452feee183ca2e8a9ad5eba3d1
Anuraag <thehray@gmail.com>
2019-10-06 10:12:21 +0100

    + property's booking api added

--- controllers/v1/property/property.controller.js
+++ controllers/v1/property/property.controller.js
@@ -10,5 +10,18 @@ module.exports = {
     } catch (error) {
       return res.serverFail(400, errorHandler(error));
     }
+  },
+
+  getPropertyBookings: async (req, res) => {
+    try {
+      // 1. fetch all properties
+      await propertyService.isValidProperty(req.params.id);
+      const property = await propertyService.getAllBookingForProperty(
+        req.params.id
+      );
+      return res.success({ property });
+    } catch (error) {
+      return res.serverFail(400, errorHandler(error));
+    }
   }
 };
--- routes/v1/property.route.js
+++ routes/v1/property.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { propertyController } = require("../../controllers/v1/property");
 
 router.get("/", propertyController.listAllProperties);
+router.get("/:id/bookings", propertyController.getPropertyBookings);
 
 module.exports = router;
